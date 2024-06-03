ea48f75e26022242280287b419c664936c5dbf99
xomp <nsk.dima@gmail.com>
2024-06-03 23:44:11 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/users', function () {
+    return User::all();
 });
